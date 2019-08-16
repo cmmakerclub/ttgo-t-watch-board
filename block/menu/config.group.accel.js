c96d7d5f4304fb74ff0a4f2dc0ddbaec9669bbb7
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-08-16 17:27:47 +0700

    reposition group accel

--- block/menu/config.group.accel.js
+++ block/menu/config.group.accel.js
@@ -4,12 +4,12 @@ module.exports = {
   icon: "/static/icons/icons8_exercise_96px.png",
   blocks: [
      'bma_acceleration_begin',
+     'bma_get_activity',
      'bma_get_direction',
      'bma_get_stepwalk',
      'bma_clear_stepwalk',
      'bma_get_accel_x',
      'bma_get_accel_y',
-     'bma_get_accel_z',
-     'bma_get_activity'
+     'bma_get_accel_z'
   ]
 };
